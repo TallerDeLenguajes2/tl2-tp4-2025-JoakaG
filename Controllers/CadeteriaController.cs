f0ddfe6670987c8add628be8cd8cbc3f8e311ccf
JoakaG <girbaujoaquina2@gmail.com>
2025-09-25 16:29:48 -0300

    luego sigo, creación de los endpoints falta el cuerpo de cada uno

--- Controllers/CadeteriaController.cs
+++ Controllers/CadeteriaController.cs
@@ -4,12 +4,53 @@ namespace CadeteriaControlador
 {
     [ApiController]
     [Route("api/[controller]")]
+    
+
+    
     public class CadeteriaController : ControllerBase
     {
-        [HttpGet]
-        public GetCadetes()
+        [HttpGet("cadetes")]
+        public ActionResult<Cadetes> GetCadetes()
         {
-            List<Cadetes> cadetes = cadeteria
+            List<Cadetes> cadetes = new();
+            return Ok(cadetes);
         }
-}
+
+        [HttpGet("pedidos")]
+        public ActionResult<Pedidos> GetPedidos()
+        {
+            List<Pedidos> pedidos = new();
+            return Ok(pedidos);
+        }
+        [HttpGet("informe")]
+        public ActionResult<string> GetInforme()
+        {
+            string informe = "Hola";
+            return Ok(informe);
+        }
+
+        [HttpPost("agregarPedido")]
+        public ActionResult<Pedidos> AgregarPedido(Pedidos pedido)
+        {
+            return Ok(pedido);
+        }
+        [HttpPut("asignarPedido")]
+        public ActionResult<bool> AsignarPedido(int idPedido, int idCadete)
+        {
+            return Ok(true);
+        }
+        [HttpPut("cambiarEstadoPedido")]
+        public ActionResult CambiarEstadoPedido(int idPedido, int NuevoEstado)
+        {
+            
+            return Ok(true);
+        }
+        [HttpPut("cambiarCadetePedido")]
+        public ActionResult<bool> CambiarCadetePedido(int idPedido, int idNuevoCadete)
+        {
+            return Ok(true);
+        }
+
+
+    }
 }
