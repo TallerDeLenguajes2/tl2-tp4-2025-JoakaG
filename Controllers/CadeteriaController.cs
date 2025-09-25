c14cbb3af79f351543c6eb2d2914ad314ecae27a
JoakaG <girbaujoaquina2@gmail.com>
2025-09-25 16:33:58 -0300

    agregue un get test

--- Controllers/CadeteriaController.cs
+++ Controllers/CadeteriaController.cs
@@ -4,49 +4,54 @@ namespace CadeteriaControlador
 {
     [ApiController]
     [Route("api/[controller]")]
-    
 
-    
     public class CadeteriaController : ControllerBase
     {
+        [HttpGet("test")]
+        public ActionResult<string> Test()
+        {
+            return Ok("OK");
+        }
+
+
         [HttpGet("cadetes")]
-        public ActionResult<Cadetes> GetCadetes()
+        public ActionResult GetCadetes()
         {
             List<Cadetes> cadetes = new();
             return Ok(cadetes);
         }
 
         [HttpGet("pedidos")]
-        public ActionResult<Pedidos> GetPedidos()
+        public ActionResult GetPedidos()
         {
             List<Pedidos> pedidos = new();
             return Ok(pedidos);
         }
         [HttpGet("informe")]
-        public ActionResult<string> GetInforme()
+        public ActionResult GetInforme()
         {
             string informe = "Hola";
             return Ok(informe);
         }
 
         [HttpPost("agregarPedido")]
-        public ActionResult<Pedidos> AgregarPedido(Pedidos pedido)
+        public ActionResult AgregarPedido(Pedidos pedido)
         {
             return Ok(pedido);
         }
         [HttpPut("asignarPedido")]
-        public ActionResult<bool> AsignarPedido(int idPedido, int idCadete)
+        public ActionResult AsignarPedido(int idPedido, int idCadete)
         {
             return Ok(true);
         }
         [HttpPut("cambiarEstadoPedido")]
         public ActionResult CambiarEstadoPedido(int idPedido, int NuevoEstado)
         {
-            
+
             return Ok(true);
         }
         [HttpPut("cambiarCadetePedido")]
-        public ActionResult<bool> CambiarCadetePedido(int idPedido, int idNuevoCadete)
+        public ActionResult CambiarCadetePedido(int idPedido, int idNuevoCadete)
         {
             return Ok(true);
         }
